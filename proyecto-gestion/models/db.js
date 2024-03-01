99cc9103782368999c015e103eac221fd9c17e76
johanbustosb <johitan120@hotmail.com>
2024-03-01 14:35:09 -0500

    se actualiza el proyecto'

--- proyecto-gestion/models/db.js
+++ proyecto-gestion/models/db.js
@@ -1,10 +1,10 @@
 
 const Sequelize = require('sequelize');
 
-// Primero definimos sequelize con los parámetros de conexión
+
 const sequelize = new Sequelize(process.env.DATABASE, process.env.DB_USER, process.env.DB_PASSWORD, {
     host: process.env.DB_HOST,
-    dialect: 'mariadb'
+    dialect: 'sql'
 });
   
 
